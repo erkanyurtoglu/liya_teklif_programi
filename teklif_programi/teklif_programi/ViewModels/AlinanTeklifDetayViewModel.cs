75e94d925a1228604dfdfb9b99ef3f6c8370b9e5
erkanyurtoglu <158731690+erkanyurtoglu@users.noreply.github.com>
2025-08-27 16:39:59 +0300

    Alınan Tekliflerime kar, maliiyet, toplam eklendi.

--- teklif_programi/teklif_programi/ViewModels/AlinanTeklifDetayViewModel.cs
+++ teklif_programi/teklif_programi/ViewModels/AlinanTeklifDetayViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using teklif_programi.Data;
 using teklif_programi.Helpers;
 using teklif_programi.Models;
+using teklif_programi.Services;
 
 namespace teklif_programi.ViewModels
 {
@@ -30,11 +33,64 @@ namespace teklif_programi.ViewModels
             }
 
             KaydetCommand = new RelayCommand(Kaydet);
+
+            UpdateTotalsText();
         }
 
         private void Kaydet()
         {
             _context.SaveChanges();
+        }
+
+        // Hesaplanan değerler
+        public decimal ToplamFiyat => Urunler.Sum(u => u.ToplamTutar);
+        public decimal KdvTutari => TeklifHesaplayici.HesaplaKdv(ToplamFiyat, Teklif.KdvOrani);
+        public decimal GenelToplam => TeklifHesaplayici.HesaplaGenelToplam(ToplamFiyat, Teklif.KdvOrani, Teklif.TeklifToplam?.PaketlemeUcreti ?? 0);
+        public decimal ToplamMaliyet => Urunler.Sum(u => u.Adet * u.Urun.MaliyetFiyati);
+        public decimal KarTutari => TeklifHesaplayici.HesaplaKarTutari(ToplamFiyat, ToplamMaliyet);
+        public decimal KarOrani => TeklifHesaplayici.HesaplaKarOrani(KarTutari, ToplamMaliyet);
+
+        private string _toplamMaliyetText = string.Empty;
+        public string ToplamMaliyetText { get => _toplamMaliyetText; set { _toplamMaliyetText = value; OnPropertyChanged(); } }
+
+        private string _karTutariText = string.Empty;
+        public string KarTutariText { get => _karTutariText; set { _karTutariText = value; OnPropertyChanged(); } }
+
+        private string _karOraniText = string.Empty;
+        public string KarOraniText { get => _karOraniText; set { _karOraniText = value; OnPropertyChanged(); } }
+
+        private string _toplamFiyatText = string.Empty;
+        public string ToplamFiyatText { get => _toplamFiyatText; set { _toplamFiyatText = value; OnPropertyChanged(); } }
+
+        private string _kdvTutariText = string.Empty;
+        public string KdvTutariText { get => _kdvTutariText; set { _kdvTutariText = value; OnPropertyChanged(); } }
+
+        private string _genelToplamText = string.Empty;
+        public string GenelToplamText { get => _genelToplamText; set { _genelToplamText = value; OnPropertyChanged(); } }
+
+        private void UpdateTotalsText()
+        {
+            ToplamMaliyetText = FormatPrice(ToplamMaliyet);
+            KarTutariText = FormatPrice(KarTutari);
+            KarOraniText = KarOrani.ToString("F2") + "%";
+            ToplamFiyatText = FormatPrice(ToplamFiyat);
+            KdvTutariText = FormatPrice(KdvTutari);
+            GenelToplamText = FormatPrice(GenelToplam);
+        }
+
+        private string FormatPrice(decimal price)
+        {
+            return price.ToString("C2", GetCultureByCurrency(Teklif.ParaBirimi));
+        }
+
+        private static CultureInfo GetCultureByCurrency(string currency)
+        {
+            return currency switch
+            {
+                "USD" => new CultureInfo("en-US"),
+                "EUR" => new CultureInfo("en-IE"),
+                _ => new CultureInfo("tr-TR"),
+            };
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
