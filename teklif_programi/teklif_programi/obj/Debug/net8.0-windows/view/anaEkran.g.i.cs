8a18378aeed667fa2e419f490f30c411032ade45
erkanyurtoglu <158731690+erkanyurtoglu@users.noreply.github.com>
2025-09-05 16:36:22 +0300

    Program Kontrol Edildi.
    Admin Teklif Oluşturuken hata oluştu.

--- teklif_programi/teklif_programi/obj/Debug/net8.0-windows/view/anaEkran.g.i.cs
+++ teklif_programi/teklif_programi/obj/Debug/net8.0-windows/view/anaEkran.g.i.cs
@@ -126,7 +126,7 @@ namespace teklif_programi.view {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/teklif_programi;component/view/anaekran.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/teklif_programi;V1.0.0.0;component/view/anaekran.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\view\anaEkran.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
