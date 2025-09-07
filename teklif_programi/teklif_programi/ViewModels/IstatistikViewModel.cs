d54022ad609cecf0816ecda60647454359d68fea
erkanyurtoglu <158731690+erkanyurtoglu@users.noreply.github.com>
2025-09-07 19:37:40 +0300

    İstatistik Ekranı Güncellendi.

--- teklif_programi/teklif_programi/ViewModels/IstatistikViewModel.cs
+++ teklif_programi/teklif_programi/ViewModels/IstatistikViewModel.cs
@@ -12,6 +12,10 @@ namespace teklif_programi.ViewModels
         public int HaftalikTeklif { get; }
         public int AylikTeklif { get; }
         public int YillikTeklif { get; }
+        public int ToplamGonderilen { get; }
+        public int ToplamKabulEdilen { get; }
+        public int ToplamReddedilen { get; }
+        public int ToplamBeklemede { get; }
         public ObservableCollection<PersonelPerformans> PersonelPerformanslari { get; }
 
         public IstatistikViewModel()
@@ -19,6 +23,10 @@ namespace teklif_programi.ViewModels
             var service = new IstatistikService();
             (GunlukTeklif, HaftalikTeklif, AylikTeklif, YillikTeklif) = service.GetTeklifSayilari();
             var list = service.GetPersonelPerformanslari();
+            ToplamGonderilen = list.Sum(p => p.Gonderilen);
+            ToplamKabulEdilen = list.Sum(p => p.KabulEdilen);
+            ToplamReddedilen = list.Sum(p => p.Reddedilen);
+            ToplamBeklemede = list.Sum(p => p.Beklemede);
             PersonelPerformanslari = new ObservableCollection<PersonelPerformans>(list);
         }
     }
